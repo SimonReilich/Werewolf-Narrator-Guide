5e9c211a7ff8d2c50db05672f0da88e2cc027228
SimonReilich <174124874+SimonReilich@users.noreply.github.com>
2025-01-05 18:27:32 +0100

    refactor: remove unused components and update index.html for new project structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div class="background-container"></div>
+	<div class="header">
+		<h1>Werewolf</h1>
+		<h3>Narrator Guide</h3>
+	</div>
+	<div class="content">
+		<p>The physical game is still required, this website is just a tool which might be helpful to the narrator,
+			especially in larger groups. <br> <br> You can either choose the roles to play with by hand or enter
+			the
+			number of players you want to play with and pick a automatically generated preset. <br> <br> If you
+			have any
+			feedback, please reach out to me. Have fun playing!</p>
+	</div>
+	<div class="button-container">
+		<button>Choose Cards</button>
+		<button>Pick Preset</button>
+	</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.background-container {
+	content: "";
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: url('/noise.png');
+	pointer-events: none;
+	z-index: -1;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.header {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+	height: 20vh;
+	position: relative;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.content {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 60vh;
+	width: 100%;
+	max-width: 40rem;
+	position: relative;
+	text-align: center;
+	margin: auto;
+}
+
+.button-container {
+	display: flex;
+	flex-direction: column-reverse;
+	align-items: center;
+	width: 100%;
+	height: calc(20vh - 1rem);
+}
+
+h1 {
+	margin-top: 4rem;
+	margin-bottom: 0rem;
 }
 </style>
