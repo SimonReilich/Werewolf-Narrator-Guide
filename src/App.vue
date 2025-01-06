bdf3759f3aa294ada10eeee47a440f9c158695db
SimonReilich <174124874+SimonReilich@users.noreply.github.com>
2025-01-06 14:25:12 +0100

    feat: implement Index page and navigation logic in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <script setup>
+import Index from './pages/Index.vue';
+import { ref } from 'vue';
+
+const renderIndex = ref(true);
+const renderCards = ref(false);
+const renderPreset = ref(false);
+
+function changePage(newPage) {
+	renderIndex.value = false;
+	renderCards.value = false;
+	renderPreset.value = false;
+
+	if (newPage === 'index') {
+		renderIndex.value = true;
+	} else if (newPage === 'cards') {
+		renderCards.value = true;
+	} else if (newPage === 'preset') {
+		renderPreset.value = true;
+	}
+}
 </script>
 
 <template>
 	<div class="background-container"></div>
-	<div class="header">
-		<h1>Werewolf</h1>
-		<h3>Narrator Guide</h3>
-	</div>
-	<div class="content">
-		<p>The physical game is still required, this website is just a tool which might be helpful to the narrator,
-			especially in larger groups. <br> <br> You can either choose the roles to play with by hand or enter
-			the
-			number of players you want to play with and pick a automatically generated preset. <br> <br> If you
-			have any
-			feedback, please reach out to me. Have fun playing!</p>
-	</div>
-	<div class="button-container">
-		<button>Choose Cards</button>
-		<button>Pick Preset</button>
-	</div>
+	<Index v-if="renderIndex" @cards="changePage('cards')" @preset="changePage('preset')" />
 </template>
 
 <style scoped>
@@ -32,40 +37,5 @@
 	background: url('/noise.png');
 	pointer-events: none;
 	z-index: -1;
-}
-
-.header {
-	display: flex;
-	flex-direction: column;
-	justify-content: flex-start;
-	align-items: center;
-	width: 100%;
-	height: 20vh;
-	position: relative;
-}
-
-.content {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 60vh;
-	width: 100%;
-	max-width: 40rem;
-	position: relative;
-	text-align: center;
-	margin: auto;
-}
-
-.button-container {
-	display: flex;
-	flex-direction: column-reverse;
-	align-items: center;
-	width: 100%;
-	height: calc(20vh - 1rem);
-}
-
-h1 {
-	margin-top: 4rem;
-	margin-bottom: 0rem;
 }
 </style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,59 @@
+<script setup>
+const emit = defineEmits(['cards', 'preset']);
+</script>
+
+<template>
+	<div class="header">
+		<h1>Werewolf</h1>
+		<h3>Narrator Guide</h3>
+	</div>
+	<div class="content">
+		<p>The physical game is still required, this website is just a tool which might be helpful to the narrator,
+			especially in larger groups. <br> <br> You can either choose the roles to play with by hand or enter
+			the
+			number of players you want to play with and pick a automatically generated preset. <br> <br> If you
+			have any
+			feedback, please reach out to me. Have fun playing!</p>
+	</div>
+	<div class="button-container">
+		<button @click="emit('cards')">Choose Cards</button>
+		<button @click="emit('preset')">Pick Preset</button>
+	</div>
+</template>
+
+<style scoped>
+.header {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	width: 100%;
+	height: 20vh;
+	position: relative;
+}
+
+.content {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 50vh;
+	width: 100%;
+	max-width: 40rem;
+	position: relative;
+	text-align: center;
+	margin: auto;
+}
+
+.button-container {
+	display: flex;
+	flex-direction: column-reverse;
+	align-items: center;
+	width: 100%;
+	height: calc(30vh - 2rem);
+}
+
+h1 {
+	margin-top: 4rem;
+	margin-bottom: 0rem;
+}
+</style>
